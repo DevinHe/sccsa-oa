faa95be6a2eaa8b7008f73c9d7330db12ed73daf
shin-ruby <shin.php@gmail.com>
2015-02-20 13:47:14 +0800

    create welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :users
 
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
-  root 'users#index'
+  root 'welcome#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
