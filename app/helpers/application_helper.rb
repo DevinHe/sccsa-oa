a3e7ccdc8ab1ddbf045c952d380a7b7f19ba9d05
shin-ruby <shin.php@gmail.com>
2015-02-26 16:26:40 +0800

    update signin style and flash messages style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,14 @@ module ApplicationHelper
   def cdistributor?
     current_user.distributor?
   end
+
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
+
 end
