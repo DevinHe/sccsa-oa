87da589331753409a707953c7a5cd337497b825d
DevinHe <huopo125@gmail.com>
2015-03-01 19:05:22 +0800

    fix

--- db/migrate/20150220085525_remove_role_for_users.rb
+++ db/migrate/20150220085525_remove_role_for_users.rb
@@ -1,6 +1,6 @@
 class RemoveRoleForUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :role
+    remove_column :users, :role, :integer
     add_column :users, :role_id, :integer
   end
 end
